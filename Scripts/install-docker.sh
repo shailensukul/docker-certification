00ca07128e666aa64a4871aa3d0b4bfee850f742
Shailen  Sukul <shailensukul@gmail.com>
2019-06-15 10:02:43 +1000

    added install docker script

---
+++ Scripts/install-docker.sh
@@ -0,0 +1,10 @@
+# ./docker-install.sh
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+systemctl status docker
+docker info
