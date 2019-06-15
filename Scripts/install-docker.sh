008fc6f08ab551175594c5637e9ffa812251c335
Shailen  Sukul <shailensukul@gmail.com>
2019-06-15 10:11:25 +1000

    upd

--- Scripts/install-docker.sh
+++ Scripts/install-docker.sh
@@ -5,6 +5,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce
+apt install -y docker-ce
 systemctl status docker
 docker info
